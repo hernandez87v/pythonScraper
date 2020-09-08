bc58aab009f878b3eb3681ab06f7eb3fdc52b957
hernandez87v <hernandez87v@hotmail.com>
2020-09-08 14:00:57 -0700

    completed scraper for Artist names beginning with Z

--- beautiful_soup.py
+++ beautiful_soup.py
@@ -3,33 +3,36 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-# Collect first page of artists’ list
-page = requests.get(
-    'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
-
-# Create a BeautifulSoup object
-soup = BeautifulSoup(page.text, 'html.parser')
-
-# Remove bottom links
-last_links = soup.find(class_='AlphaNav')
-last_links.decompose()
-
 # Create a file to write to, add headers row
 f = csv.writer(open('z-artist-names.csv', 'w'))
 f.writerow(['Name', 'Link'])
 
-# Pull all text from the BodyText div
-artist_name_list = soup.find(class_='BodyText')
+pages = []
 
-# Pull text from all instances of <a> tag within BodyText div
-artist_name_list_items = artist_name_list.find_all('a')
+# Iterate over all pages "Z"
+for i in range(1, 5):
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + \
+        str(i) + '.htm'
+    pages.append(url)
 
-# Use .contents to pull out the <a> tag’s children
-for artist_name in artist_name_list_items:
-    names = artist_name.contents[0]
-    links = 'https://web.archive.org' + artist_name.get('href')
-    print(names)
-    print(links)
+for item in pages:
+    page = requests.get(item)
+    # Create a BeautifulSoup object
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-    # Add each artist’s name and associated link to a row
-    f.writerow([names, links])
+    # Remove bottom links
+    last_links = soup.find(class_='AlphaNav')
+    last_links.decompose()
+
+    # Pull all text from the BodyText div
+    artist_name_list = soup.find(class_='BodyText')
+    # Pull text from all instances of <a> tag within BodyText div
+    artist_name_list_items = artist_name_list.find_all('a')
+
+    # Use .contents to pull out the <a> tag’s children
+    for artist_name in artist_name_list_items:
+        names = artist_name.contents[0]
+        links = 'https://web.archive.org' + artist_name.get('href')
+
+        # Add each artist’s name and associated link to a row
+        f.writerow([names, links])
---
+++ practice.py
@@ -0,0 +1,8 @@
+# Nested for loops
+num_list = [1, 2, 3]
+alpha_list = ['a', 'b', 'c']
+
+for number in num_list:
+    print(number)
+    for letter in alpha_list:
+        print(letter)
